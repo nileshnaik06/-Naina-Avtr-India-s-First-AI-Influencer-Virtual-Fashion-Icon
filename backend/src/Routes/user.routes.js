d0377ee965d26d02e184b67c8ede6560ae5c4087
nileshnaik06 <you@example.com>
2025-07-24 13:33:36 +0530

    backend

--- backend/src/Routes/user.routes.js
+++ backend/src/Routes/user.routes.js
@@ -33,5 +33,52 @@ router.post("/user", async (req, res) => {
 
 })
 
+router.get("/user", async (req, res) => {
+    try {
+        const { userEmail } = req.query; // Use req.query for GET params
+
+        if (!userEmail) {
+            return res.status(400).json({ message: "Email is required" });
+        }
+
+        const userExist = await userModel.findOne({ userEmail });
+
+        if (!userExist) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(200).json({
+            user: userExist,
+            message: "User found",
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error",
+            error: error.message,
+        });
+    }
+});
+
+
+router.delete("/user/:id", async (req, res) => {
+    try {
+        const { _id } = req.query;
+        const deletedUser = await userModel.findByIdAndDelete(_id);
+
+        if (!deletedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(200).json({
+            message: "User deleted successfully",
+            user: deletedUser,
+        });
+    } catch (error) {
+        res.status(400).json({
+            message: "Failed to delete user",
+            error: error.message,
+        });
+    }
+})
 
 module.exports = router
